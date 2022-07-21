7ca90073c7694b00bd156640cf3a30051a904ce3
nhistory <nhistory@naver.com>
2022-07-20 22:03:15 -0300

    Add PostController, 404.html and post.html

---
+++ src/main/java/com/example/springbootblog/controllers/PostController.java
@@ -0,0 +1,32 @@
+package com.example.springbootblog.controllers;
+
+import com.example.springbootblog.models.Post;
+import com.example.springbootblog.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
+
+@Controller
+public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+    @GetMapping("/posts/{id}")
+    public String getPost(@PathVariable Long id, Model model) {
+        // find the post by id
+        Optional<Post> optionalPost = postService.getById(id);
+        // if the post exists, then show it into the model
+        if (optionalPost.isPresent()) {
+            Post post = optionalPost.get();
+            model.addAttribute("post", post);
+            return "post";
+        } else {
+            return "404";
+        }
+    }
+}
