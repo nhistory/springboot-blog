e55d98c33f76332bcc94457009b6fb81b7c46f3e
nhistory <nhistory@naver.com>
2022-07-27 19:54:50 -0300

    Add security tag on the home.html

--- src/main/java/com/example/springbootblog/config/WebSecurityConfig.java
+++ src/main/java/com/example/springbootblog/config/WebSecurityConfig.java
@@ -5,11 +5,18 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class WebSecurityConfig {
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     private static final String[] WHITELIST = {
             "/register",
--- src/main/java/com/example/springbootblog/services/AccountService.java
+++ src/main/java/com/example/springbootblog/services/AccountService.java
@@ -3,6 +3,7 @@ package com.example.springbootblog.services;
 import com.example.springbootblog.models.Account;
 import com.example.springbootblog.repositories.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,9 +12,13 @@ import java.util.Optional;
 public class AccountService {
 
     @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
     private AccountRepository accountRepository;
 
     public Account save(Account account) {
+        account.setPassword(passwordEncoder.encode(account.getPassword()));
         return accountRepository.save(account);
     }
 
